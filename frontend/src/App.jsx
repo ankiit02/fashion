cde7f581d98740561e01e48ef80dc517c37c4ab4
ankiit02 <0447.ankit@gmail.com>
2025-04-02 11:37:49 +0530

    UPDATE APP

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
-import Home from './pages/home'
-import Collection from './pages/collection'
-import About from './pages/about'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
 import Contact from './pages/Contact'
-import Cart from './pages/cart'
-import Login from './pages/login'
-import PlaceOrder from './pages/placeOrder'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import Product from './pages/product'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer  } from 'react-toastify';
 import Verify from './pages/Verify'
 // import 'react-toastify/dist/ReactToastify.css';
 
